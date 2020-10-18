433d79989dcc93f904c5f470d8dfaf33421a54c0
NicolaCraperi <crape@97@gmail.com>
2020-10-18 12:28:47 +0200

    chore: init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 640,
+    height: 720,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('src/game.html')
+  win.webContents.openDevTools()
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
